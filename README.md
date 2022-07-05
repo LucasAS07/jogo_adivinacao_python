f8065c04485eeb2890083aad62c55eb85e9a365f
LucasAS07 <lucas-oliveira25@outlook.com>
2022-07-05 08:04:32 -0300

    Subindo porjeto
    Subindo primeira versão do jogo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jogo_adivinacao_python
  Jogo de adivinhação em python
+
+ Criando um jogo de adivinhação em Python para treinamento da linguagem
+ e fixação de alguns conceitos.
---
+++ jogo/jogo_adivinhacao.py
@@ -0,0 +1,16 @@
+print('********************************')
+print('BEM VINDO AO JOGO DE ADIVINHAÇÃO')
+print('********************************')
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu numero: ")
+print('Você digitou: ', chute_str)
+chute = int(chute_str)
+
+if(numero_secreto == chute):
+    print('Você acertou!')
+else:
+    print('Você errou!')
+
+print('Fim de Jogo!')
