1fdbf8ef4fa8ddbc0f7b2ae3ad4545ea85e77631
LucasAS07 <lucas-oliveira25@outlook.com>
2022-07-13 14:11:59 -0300

    Incrementando o jogo
    Adicionada novas condições ao jogo.

--- jogo/jogo_adivinhacao.py
+++ jogo/jogo_adivinhacao.py
@@ -3,14 +3,20 @@ print('BEM VINDO AO JOGO DE ADIVINHAÇÃO')
 print('********************************')
 
 numero_secreto = 42
+numero_tentativas = 5
+for rodada in range(1, numero_tentativas + 1):
+    print(f'Tentativa {numero_tentativas} de 5!')
+    chute_str = input("Digite o seu numero: ")
+    print('Você digitou: ', chute_str)
+    chute = int(chute_str)
 
-chute_str = input("Digite o seu numero: ")
-print('Você digitou: ', chute_str)
-chute = int(chute_str)
-
-if(numero_secreto == chute):
-    print('Você acertou!')
-else:
-    print('Você errou!')
+    if(numero_secreto == chute):
+        print('Você acertou!')
+    else:
+        if(chute > numero_secreto):
+            print('Você errou chutou para cima do numero!')
+        elif(chute < numero_secreto):
+            print('Você errou chutou para baixo do numero! ')
+    numero_tentativas = numero_tentativas - 1
 
 print('Fim de Jogo!')
